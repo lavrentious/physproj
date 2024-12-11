57e8e60d719ebfff0d4aad159758e0304aff4455
lavrentious <lavrent0@yandex.ru>
2024-12-11 20:00:06 +0300

    build: specify base

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: 'src',
+  root: "src",
+  base: "/physproj/",
   build: {
-    outDir: '../dist',
-    target: 'es6',
+    outDir: "../dist",
+    target: "es6",
   },
   server: {
     host: true, // Use '0.0.0.0' to allow access from local network
