c821ba43a3e553ed572eef7cd8f0061122af8eeb
lavrentious <lavrent0@yandex.ru>
2024-12-12 11:22:43 +0300

    refactor: class

--- src/index.ts
+++ src/index.ts
@@ -3,143 +3,198 @@ import { App } from "./App";
 import { RuntimeConfig } from "./runtime_config";
 import { PlacementType } from "./services/placement_service";
 
-async function main() {
-  // init html
-  const pixiContainer = document.getElementById("pixi");
-  if (!pixiContainer) {
-    throw new Error("Parent block not found");
+class Index {
+  app: App;
+  pixiContainer: HTMLElement;
+  datGui: dat.GUI;
+
+  physicsSwitchButton: HTMLButtonElement;
+  pauseButton: HTMLButtonElement;
+  restartButton: HTMLButtonElement;
+  applyButton: HTMLButtonElement;
+  honeyButton: HTMLButtonElement;
+  iceButton: HTMLButtonElement;
+  tableButton: HTMLButtonElement;
+  runtimeConfig: RuntimeConfig = new RuntimeConfig();
+
+  private getElementByIdOrThrow<T extends HTMLElement>(id: string): T {
+    const element = document.getElementById(id) as T;
+    if (!element) {
+      throw new Error(`Element with id ${id} not found`);
+    }
+    return element;
   }
 
-  // Physics-Button logic
-  document
-    .getElementById("switcher-button")
-    ?.addEventListener("click", function (event) {
+  constructor() {
+    this.app = new App();
+    this.datGui = new dat.GUI({ autoPlace: false });
+    this.pixiContainer = this.getElementByIdOrThrow("pixi");
+
+    // navbar buttons
+    this.physicsSwitchButton = this.getElementByIdOrThrow("switcher-button");
+    this.pauseButton = this.getElementByIdOrThrow("pause-button");
+    this.restartButton = this.getElementByIdOrThrow("restart-button");
+
+    // config menu buttons
+    this.tableButton = this.getElementByIdOrThrow("table-button");
+    this.honeyButton = this.getElementByIdOrThrow("honey-button");
+    this.iceButton = this.getElementByIdOrThrow("ice-button");
+    this.applyButton = this.getElementByIdOrThrow("apply-button");
+  }
+
+  // listeners
+  // - navbar buttons
+  private physicsSwitchButtonClickListener(event: MouseEvent) {
+    event.preventDefault();
+    this.app.togglePhysics();
+    this.physicsSwitchButton.classList.toggle(
+      "btn-success",
+      this.app.getPhysics(),
+    );
+    this.physicsSwitchButton.classList.toggle(
+      "btn-danger",
+      !this.app.getPhysics(),
+    );
+  }
+
+  private pauseButtonClickListener(event?: MouseEvent) {
+    if (event) {
       event.preventDefault();
-      app.togglePhysics();
-      this.classList.toggle("btn-success", app.getPhysics());
-      this.classList.toggle("btn-danger", !app.getPhysics());
-    });
+    }
+    this.app.togglePause();
+    console.log(this.app.getPause());
 
-  // Pause-Button logic
-  document
-    .getElementById("pause-button")
-    ?.addEventListener("click", function (event) {
-      event.preventDefault();
-      app.togglePause();
-      this.children[0].classList.toggle("bi-play-fill", app.getPause());
-      this.children[0].classList.toggle("bi-pause", !app.getPause());
-    });
+    this.pauseButton.children[0].classList.toggle(
+      "bi-play-fill",
+      this.app.getPause(),
+    );
+    this.pauseButton.children[0].classList.toggle(
+      "bi-pause",
+      !this.app.getPause(),
+    );
+  }
 
-  // Restart-Button logic
-  document
-    .getElementById("restart-button")
-    ?.addEventListener("click", function (event) {
-      event.preventDefault();
-      app.restart();
-      console.log(runtimeConfig);
-    });
+  private restartButtonClickListener(event: MouseEvent) {
+    event.preventDefault();
+    this.app.restart();
+  }
 
-  // Apply-Button logic
-  document
-    .getElementById("apply-button")
-    ?.addEventListener("click", function (event) {
-      event.preventDefault();
-      app.setRuntimeConfig(runtimeConfig);
-      console.log(runtimeConfig);
-      document.getElementById("close-offcanvas")?.click();
-      document
-        .getElementById("pause-button")
-        ?.children[0].classList.toggle("bi-play-fill", app.getPause());
-      document
-        .getElementById("pause-button")
-        ?.children[0].classList.toggle("bi-pause", !app.getPause());
-    });
+  private updateTableFriction(frictionCoef?: number) {
+    if (frictionCoef) {
+      this.runtimeConfig.frictionCoef = frictionCoef;
+    }
+    console.log("new friction", this.runtimeConfig.frictionCoef);
 
-  // Honey-Button logic
-  document
-    .getElementById("honey-button")
-    ?.addEventListener("click", function (event) {
-      event.preventDefault();
-      runtimeConfig.frictionCoef = 0.3;
-      app.setRuntimeConfig(runtimeConfig);
-      document.getElementById("close-offcanvas")?.click();
-      document
-        .getElementById("pause-button")
-        ?.children[0].classList.toggle("bi-play-fill", app.getPause());
-      document
-        .getElementById("pause-button")
-        ?.children[0].classList.toggle("bi-pause", !app.getPause());
-    });
+    this.datGui.updateDisplay();
+    this.app.setRuntimeConfig(this.runtimeConfig);
+    document.getElementById("close-offcanvas")?.click();
+    this.pauseButton.children[0].classList.toggle(
+      "bi-play-fill",
+      this.app.getPause(),
+    );
+    this.pauseButton.children[0].classList.toggle(
+      "bi-pause",
+      !this.app.getPause(),
+    );
+  }
 
-  // Ice-Button logic
-  document
-    .getElementById("ice-button")
-    ?.addEventListener("click", function (event) {
-      event.preventDefault();
-      runtimeConfig.frictionCoef = 0.002;
-      app.setRuntimeConfig(runtimeConfig);
+  // - table types
+  private iceButtonClickListener(event: MouseEvent) {
+    event.preventDefault();
+    this.updateTableFriction(0.002);
+  }
 
-      document.getElementById("close-offcanvas")?.click();
-      document
-        .getElementById("pause-button")
-        ?.children[0].classList.toggle("bi-play-fill", app.getPause());
-      document
-        .getElementById("pause-button")
-        ?.children[0].classList.toggle("bi-pause", !app.getPause());
-    });
+  private tableButtonClickListener(event: MouseEvent) {
+    event.preventDefault();
+    this.updateTableFriction(0.02);
+  }
 
-  // Table-Button logic
-  document
-    .getElementById("table-button")
-    ?.addEventListener("click", function (event) {
-      event.preventDefault();
-      runtimeConfig.frictionCoef = 0.02;
-      app.setRuntimeConfig(runtimeConfig);
-      document.getElementById("close-offcanvas")?.click();
-      document
-        .getElementById("pause-button")
-        ?.children[0].classList.toggle("bi-play-fill", app.getPause());
-      document
-        .getElementById("pause-button")
-        ?.children[0].classList.toggle("bi-pause", !app.getPause());
-    });
+  private honeyButtonClickListener(event: MouseEvent) {
+    event.preventDefault();
+    this.updateTableFriction(0.3);
+  }
 
-  const runtimeConfig = new RuntimeConfig();
+  // - config menu apply button
+  private applyButtonClickListener(event: MouseEvent) {
+    event.preventDefault();
+    this.updateTableFriction(); // apply current config
+  }
 
-  let gui = new dat.GUI({ autoPlace: false });
-  let customContainer = document.getElementById("dat-gui-container");
-  if (customContainer) customContainer.appendChild(gui.domElement);
-  // Define control elements here
-  const guiPhysicsFolder = gui.addFolder("Физика");
-  guiPhysicsFolder
-    .add(runtimeConfig, "G")
-    .min(0.1)
-    .max(20)
-    .step(0.1)
-    .name("Постоянная G");
-  guiPhysicsFolder
-    .add(runtimeConfig, "frictionCoef")
-    .min(0.001)
-    .max(1)
-    .step(0.01)
-    .name("Коэф. трения");
-  guiPhysicsFolder.open();
+  private initListeners() {
+    this.physicsSwitchButton.addEventListener(
+      "click",
+      this.physicsSwitchButtonClickListener.bind(this),
+    );
+    this.pauseButton.addEventListener(
+      "click",
+      this.pauseButtonClickListener.bind(this),
+    );
+    this.restartButton.addEventListener(
+      "click",
+      this.restartButtonClickListener.bind(this),
+    );
+    this.tableButton.addEventListener(
+      "click",
+      this.tableButtonClickListener.bind(this),
+    );
+    this.honeyButton.addEventListener(
+      "click",
+      this.honeyButtonClickListener.bind(this),
+    );
+    this.iceButton.addEventListener(
+      "click",
+      this.iceButtonClickListener.bind(this),
+    );
+    this.applyButton.addEventListener(
+      "click",
+      this.applyButtonClickListener.bind(this),
+    );
+  }
 
-  const guiSimulationFolder = gui.addFolder("Симуляция");
-  guiSimulationFolder
-    .add(runtimeConfig, "ballsNumber")
-    .min(1)
-    .max(15)
-    .step(1)
-    .name("Кол-во шаров");
-  guiSimulationFolder
-    .add(runtimeConfig, "placementType", PlacementType)
-    .name("Тип расстановки");
-  guiSimulationFolder.open();
+  private initDatGui() {
+    const customContainer = document.getElementById("dat-gui-container");
+    if (customContainer) customContainer.appendChild(this.datGui.domElement);
+    // Define control elements here
+    const guiPhysicsFolder = this.datGui.addFolder("Физика");
+    guiPhysicsFolder
+      .add(this.runtimeConfig, "G")
+      .min(0.1)
+      .max(20)
+      .step(0.1)
+      .name("Постоянная G");
+    guiPhysicsFolder
+      .add(this.runtimeConfig, "frictionCoef")
+      .min(0.001)
+      .max(1)
+      .step(0.01)
+      .name("Коэф. трения");
+    guiPhysicsFolder.open();
 
-  const app = new App();
-  await app.init(pixiContainer);
-  app.run();
+    const guiSimulationFolder = this.datGui.addFolder("Симуляция");
+    guiSimulationFolder
+      .add(this.runtimeConfig, "ballsNumber")
+      .min(1)
+      .max(15)
+      .step(1)
+      .name("Кол-во шаров");
+    guiSimulationFolder
+      .add(this.runtimeConfig, "placementType", PlacementType)
+      .name("Тип расстановки");
+    guiSimulationFolder.open();
+  }
+
+  public async init() {
+    this.initListeners();
+    this.initDatGui();
+
+    await this.app.init(this.pixiContainer);
+    this.app.run();
+  }
+}
+
+async function main() {
+  const index = new Index();
+  await index.init();
 }
 
 main();
